6b5605ad52ab89e17753272ebd9323b17224c1db
AxelMontlahuc <axel.montlahuc@proton.me>
2025-08-09 12:48:09 -0400

    Added visual mode!

--- main.py
+++ main.py
@@ -31,9 +31,9 @@ class App(badge.BaseApp):
         }
         self.rickroll = [("G#", 0.2), ("F#", 0.2), ("F", 0.2), ("D#", 0.2), ("C#", 0.2), ("C#", 0.2), ("C#", 0.2), ("C#", 0.2), ("C#", 0.2), ("D#", 0.2), ("D#", 0.2), ("D#", 0.2), ("D#", 0.2), ("D#", 0.2), ("D#", 0.2), ("G#", 0.2), ("G#", 0.2), ("G#", 0.2), ("G#", 0.2), ("D#", 0.2), ("D#", 0.2), ("D#", 0.2), ("D#", 0.2), ("D#", 0.2), ("D#", 0.2), ("F", 0.2), ("F", 0.2), ("F", 0.2), ("F", 0.2), ("F", 0.2), ("G#", 0.2), ("F#", 0.2), ("F", 0.2), ("D#", 0.2), ("C#", 0.2), ("C#", 0.2), ("C#", 0.2), ("C#", 0.2), ("C#", 0.2), ("D#", 0.2), ("D#", 0.2), ("D#", 0.2), ("D#", 0.2), ("D#", 0.2), ("D#", 0.2), ("G#", 0.2), ("G#", 0.2), ("G#", 0.2), ("G#", 0.2), ("D#", 0.2), ("D#", 0.2), ("D#", 0.2), ("D#", 0.2), ("D#", 0.2), ("D#", 0.2), ("F", 0.2), ("F", 0.2), ("F", 0.2), ("F", 0.2), ("F", 0.2)]
         self.carelesswhisper = [("E5", 0.5), ("D5", 0.3), ("A", 0.5), ("F", 0.5), ("E5", 0.8), ("D5", 0.3), ("A", 0.5), ("F", 0.8), ("A", 0.3), ("C5", 0.5), ("A#", 0.3), ("F", 0.5), ("D", 0.5), ("C5", 0.8), ("A#", 0.3), ("F", 0.3), ("C", 0.8)]
-        self.musics = [("Never Gonna Give You Up", self.rickroll), ("Careless Whisper", self.carelesswhisper)]
+        self.musics = [("Never Gonna Give You Up", self.rickroll, "rickroll.pbm"), ("Careless Whisper", self.carelesswhisper, "carelesswhisper.pbm")]
 
-
+        self.mode = "text"
 
         self.menuSize = len(self.musics)
         self.menuEntry = 0
@@ -41,9 +41,24 @@ class App(badge.BaseApp):
 
         self.playing = False
 
-    def render(self):
-        badge.display.rect(0, 32 + 18*(1+self.oldEntry), 200, 18, 1)
-        badge.display.rect(0, 32 + 18*(1+self.menuEntry), 200, 18, 0)
+    def renderUI(self):
+        if self.mode == "text":
+            badge.display.fill(1)
+            badge.display.nice_text("Music Player", 0, 0, 32)
+            for music in self.musics:
+                badge.display.nice_text(music[0], 0, 32 + 18*(1+self.musics.index(music)))
+        elif self.mode == "icon":
+            badge.display.fill(1)
+            for music in self.musics:
+                badge.display.blit(badge.display.import_pbm(f"/apps/transmitter/{music[2]}"), 14+(self.musics.index(music) % 3)*(48+14), 14+(self.musics.index(music) // 3)*(48+14))
+
+    def renderSelection(self):
+        if self.mode == "text":
+            badge.display.rect(0, 32 + 18*(1+self.oldEntry), 200, 18, 1)
+            badge.display.rect(0, 32 + 18*(1+self.menuEntry), 200, 18, 0)
+        elif self.mode == "icon":
+            badge.display.rect(14+(self.oldEntry % 3)*(48+14), 14+(self.oldEntry // 3)*(48+14), 48, 48, 1)
+            badge.display.rect(14+(self.menuEntry % 3)*(48+14), 14+(self.menuEntry // 3)*(48+14), 48, 48, 0)
         badge.display.show()
 
     def pause(self):
@@ -60,10 +75,7 @@ class App(badge.BaseApp):
         self.playing = False
 
     def on_open(self):
-        badge.display.fill(1)
-        badge.display.nice_text("Music Player", 0, 0, 32)
-        for music in self.musics:
-            badge.display.nice_text(music[0], 0, 32 + 18*(1+self.musics.index(music)))
+        self.renderUI()
 
     def loop(self):
         if badge.input.get_button(badge.input.Buttons.SW7):
@@ -83,7 +95,16 @@ class App(badge.BaseApp):
             else:
                 self.play()
                 self.playing = True
+        if badge.input.get_button(badge.input.Buttons.SW14):
+            if self.mode == "text":
+                self.mode = "icon"
+                self.renderUI()
+                self.renderSelection()
+            else:
+                self.mode = "text"
+                self.renderUI()
+                self.renderSelection()
 
         if self.oldEntry != self.menuEntry:
-            self.render()
+            self.renderSelection()
         self.oldEntry = self.menuEntry
